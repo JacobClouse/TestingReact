004ceece0944355fa3a636be48f9687c7fae896f
Jacob Clouse <jacobac00@gmail.com>
2017-01-11 21:24:42 -0600

    Add Transitions to about page

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -50225,6 +50225,20 @@ module.exports = {
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if (!confirm("Are you sure you want to see about?")) {
+                transition.abort();
+            } else {
+                callback();
+            }
+        },
+        willTransitionFrom: function(transition, component) {
+            if (!confirm("Are you sure you want to leave?")) {
+                transition.abort();
+            }
+        }
+    },
     render: function() {
         return (
             React.createElement("div", null, 
